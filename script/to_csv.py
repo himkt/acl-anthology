6e97bf10ec19451a4e8b9d9d74afc0a7df8d0396
himkt <himkt@cookpad.com>
2020-07-05 18:09:25 +0900

    Use typer

--- script/to_csv.py
+++ script/to_csv.py
@@ -1,12 +1,14 @@
-import requests
 import xml.etree.ElementTree as etree
 from typing import Dict
 
 import pandas
+import requests
+import typer
 
 
-ACL_ANTHOLOGY_DATA_URL = "https://raw.githubusercontent.com/acl-org/acl-anthology/master/data/xml/{year}.{conference}.xml"
+ACL_ANTHOLOGY_DATA_URL = "https://raw.githubusercontent.com/acl-org/acl-anthology/master/data/xml/{year}.{conference}.xml"  # NOQA
 ACL_ANTHOLOGY_HOMEPAGE_URL = "https://www.aclweb.org/anthology/{path}"
+OUTPUT_FILE_NAME = "{conference}.{year}.csv"
 
 
 def parse_child(element: 'xml.etree.ElementTree.Element') -> Dict[str, str]:
@@ -14,8 +16,7 @@ def parse_child(element: 'xml.etree.ElementTree.Element') -> Dict[str, str]:
 
     for tag in element:
         if tag.tag in ("title", "abstract"):
-            #
-            # tag.itertext for dealing with fixed-case
+            # NOTE: For <fixed-case>, it calls itertext.
             #
             # <title><fixed-case>X</fixed-case>XXXX</title>
             #        ^^^^^^^^^^^^^^^^^^^^^^^^^^
@@ -23,9 +24,12 @@ def parse_child(element: 'xml.etree.ElementTree.Element') -> Dict[str, str]:
             body[tag.tag] = ''.join(t for t in tag.itertext())
 
         if tag.tag == "author":
+            # NOTE: First or last name could be None.
+            #
+            # ref. https://www.aclweb.org/anthology/2020.acl-main.521/
+            #
             author_dict = {t.tag: t.text for t in tag}
-            # NOTE: https://www.aclweb.org/anthology/2020.acl-main.521/
-            author_name = " ".join([author_dict["first"] or "", author_dict["last"] or ""])
+            author_name = " ".join(author_dict.get("first", ""), author_dict.get("last", ""))
             body["authors"].append(author_name)
             
         if tag.tag == "url":
@@ -38,7 +42,7 @@ def parse_child(element: 'xml.etree.ElementTree.Element') -> Dict[str, str]:
     return body
 
 
-def fetch(conference: str, year: int):
+def fetch(conference: str, year: int) -> pandas.DataFrame:
     url = ACL_ANTHOLOGY_DATA_URL.format(conference=conference, year=year)
     response = requests.get(url)
     tree = etree.fromstring(response.content)
@@ -46,6 +50,19 @@ def fetch(conference: str, year: int):
     return data[["Title", "Author", "Abstract", "Url"]]
 
 
+def download(conference: str = "acl", year: int = "2020") -> None:
+    """
+    Args:
+
+        conference: name of conference, should be lowercase.
+
+        year: year of a conference.
+
+    """
+    print("conference: ", conference, ", year: ", year)
+    data = fetch(conference, year)
+    data.to_csv(OUTPUT_FILE_NAME.format(conference=conference, year=year), index=False)
+
+
 if __name__ == "__main__":
-    data = fetch("acl", 2020)
-    data.to_csv("acl.2020.csv", index=False)
+    typer.run(download)
