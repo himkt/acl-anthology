1ee8c1f1f552db29dde64d6eccf39d83b9c5af9e
himkt <himkt@klis.tsukuba.ac.jp>
2021-11-06 21:55:18 +0900

    Logging configuration

--- script/to_csv.py
+++ script/to_csv.py
@@ -1,9 +1,13 @@
 from argparse import ArgumentParser
+from logging import basicConfig
+from logging import INFO
 
 from acl_anthology import AclAnthologyDownloader
 
 
 if __name__ == "__main__":
+    basicConfig(level=INFO, format="[%(asctime)s] %(message)s")
+
     parser = ArgumentParser()
     parser.add_argument("--conference", type=str, default="acl")
     parser.add_argument("--year", type=int, default=2021)
